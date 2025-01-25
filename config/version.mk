SkylineUI-Reborn/platform_vendor_aosp

config: version: Simplify build date creation
 * Remove unused CUSTOM_BUILD_DATE_UTC too

Change-Id: Ifc7497725025b0c9762e24190853e917e766e3bd

config/version.mk
@@ -1,10 +1,4 @@
-CUSTOM_DATE_YEAR := $(shell date -u +%Y)
-CUSTOM_DATE_MONTH := $(shell date -u +%m)
-CUSTOM_DATE_DAY := $(shell date -u +%d)
-CUSTOM_DATE_HOUR := $(shell date -u +%H)
-CUSTOM_DATE_MINUTE := $(shell date -u +%M)
-CUSTOM_BUILD_DATE_UTC := $(shell date -d '$(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONTH)-$(CUSTOM_DATE_DAY) $(CUSTOM_DATE_HOUR):$(CUSTOM_DATE_MINUTE) UTC' +%s)
-CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)-$(CUSTOM_DATE_HOUR)$(CUSTOM_DATE_MINUTE)
+CUSTOM_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
 
 CUSTOM_PLATFORM_VERSION := 15.0
 

Krishna-s-Hub/android_vendor_pixelage

config/version.mk
@@ -1,9 +1,4 @@
-PIXELAGE_DATE_YEAR := $(shell date -u +%Y)
-PIXELAGE_DATE_MONTH := $(shell date -u +%m)
-PIXELAGE_DATE_DAY := $(shell date -u +%d)
-PIXELAGE_DATE_HOUR := $(shell date -u +%H)
-PIXELAGE_DATE_MINUTE := $(shell date -u +%M)
-PIXELAGE_BUILD_DATE := $(PIXELAGE_DATE_YEAR)$(PIXELAGE_DATE_MONTH)$(PIXELAGE_DATE_DAY)-$(PIXELAGE_DATE_HOUR)$(PIXELAGE_DATE_MINUTE)
+PIXELAGE_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
 
 PIXELAGE_BUILDTYPE ?= UNOFFICIAL
 