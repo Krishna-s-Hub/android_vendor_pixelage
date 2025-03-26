f71a6b65867199227929bca531b198a73e19a91d
Michael Bestas <mkbestas@lineageos.org>
2025-03-26 19:25:20 +0200

    soong: move EGL display array config to select
    Change-Id: Ia03dd6b49637ed3b7432df26824fcbf01d92106b

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -53,7 +53,6 @@ SOONG_CONFIG_pixelageGlobalVars += \
     target_trust_usb_control_path \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
-    uses_egl_display_array \
     uses_oplus_camera
 
 ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)
@@ -68,7 +67,6 @@ SOONG_CONFIG_pixelageGlobalVars_camera_override_format_from_reserved := $(TARGET
 SOONG_CONFIG_pixelageGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
 SOONG_CONFIG_pixelageGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_pixelageGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
-SOONG_CONFIG_pixelageGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_pixelageGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 
 # Set default values
