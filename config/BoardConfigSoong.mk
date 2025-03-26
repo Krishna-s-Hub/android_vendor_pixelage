b3acfd18093cbef6ab02b6ce8867adf1d80c7904
Michael Bestas <mkbestas@lineageos.org>
2025-03-26 11:30:53 +0200

    soong: move extended compress format config to select
    Change-Id: Id56bad89607cd2961a571c148ec40e9334f0a6ce

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -67,7 +67,6 @@ SOONG_CONFIG_pixelageNvidiaVars += \
 
 SOONG_CONFIG_NAMESPACES += pixelageQcomVars
 SOONG_CONFIG_pixelageQcomVars += \
-    supports_extended_compress_format \
     uses_pre_uplink_features_netmgrd
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
@@ -86,7 +85,6 @@ SOONG_CONFIG_pixelageGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRAL
 SOONG_CONFIG_pixelageGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_pixelageGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_pixelageNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
-SOONG_CONFIG_pixelageQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_pixelageQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 
 # Set default values
