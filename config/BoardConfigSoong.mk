7046310ff385bb541f0db22ed06481a03ffcdc89
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-04-28 10:27:23 +0545

    config: Move libcameraservice extension lib to select()
    Change-Id: I102a3c94f3b3facd123c238c0530e179794fcf1d

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -34,7 +34,6 @@ SOONG_CONFIG_pixelageGlobalVars += \
     camera_needs_client_info_lib_oplus \
     camera_override_format_from_reserved \
     target_camera_package_name \
-    target_libcameraservice_ext_lib \
     target_init_vendor_lib \
     target_power_libperfmgr_mode_extension_lib \
     target_trust_usb_control_path \
@@ -52,7 +51,6 @@ SOONG_CONFIG_pixelageGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED ?= false
-TARGET_CAMERA_SERVICE_EXT_LIB ?= libcameraservice_ext_lib
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB ?= libperfmgr-ext
 TARGET_TRUST_USB_CONTROL_PATH ?= /proc/sys/kernel/deny_new_usb
@@ -64,11 +62,15 @@ SOONG_CONFIG_pixelageGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADD
 SOONG_CONFIG_pixelageGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_pixelageGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 SOONG_CONFIG_pixelageGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
-SOONG_CONFIG_pixelageGlobalVars_target_libcameraservice_ext_lib := $(TARGET_CAMERA_SERVICE_EXT_LIB)
 SOONG_CONFIG_pixelageGlobalVars_target_power_libperfmgr_mode_extension_lib := $(TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB)
 SOONG_CONFIG_pixelageGlobalVars_target_trust_usb_control_path := $(TARGET_TRUST_USB_CONTROL_PATH)
 SOONG_CONFIG_pixelageGlobalVars_target_trust_usb_control_enable := $(TARGET_TRUST_USB_CONTROL_ENABLE)
 SOONG_CONFIG_pixelageGlobalVars_target_trust_usb_control_disable := $(TARGET_TRUST_USB_CONTROL_DISABLE)
+
+# libcameraservice extension
+ifneq ($(TARGET_CAMERA_SERVICE_EXT_LIB),)
+    $(call soong_config_set,libcameraservice,ext_lib,$(TARGET_CAMERA_SERVICE_EXT_LIB))
+endif
 
 # Lineage Health HAL
 ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)
